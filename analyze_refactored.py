e87c8029e192c8cfffe5a0f2a32cc4688f83668d
Timothy Lu <lutimoth@gmail.com>
2023-11-07 23:48:50 -0500

    tim best try

---
+++ analyze_refactored.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# Read in a data file
+df = pd.read_csv('data/raw/shopping_behavior_updated.csv')
+
+# calculate summary statistics on the Purchase Amount column
+# TODO: Is there a way to encapsulate all this functionality
+# TODO: in one function call?
+
+print("Summary statistics on Purchase Amount (USD)")
+print(df['Purchase Amount (USD)'].describe())
+
+# calculate summary statistics on the Age column
+# TODO: Is there a way to encapsulate all this functionality
+# TODO: in one function call?
+
+print("Summary statistics on Age")
+print(df['Age'].describe())
+
+# summary statistics
+# TODO: is there another function we can use to calculate metrics on groups?
+
+print("Seasonal Summary Statistics on Purchase Amount (USD)")
+print(df.groupby('Season')['Purchase Amount (USD)'].describe())
+
+# keep all columns except for "Customer ID", & "Discount Applied"
+# TODO: is there a more efficient way to exclude columns in your dataset?
+df = df.drop(["Customer ID","Discount Applied"], axis=1)
+
+# figure out most popular payment method in NY
+# TODO: is there anyway we could modularize this behavior to apply to all
+# TODO: possible states? (OR possibly use a pandas function that does this
+# TODO: for us already?)
+most_frequent_method = {}
+
+for state in df['Location'].unique():
+    most_frequent_method[state] = df[df['Location'] == state]['Payment Method'].value_counts().index[0]
+
+
+print(most_frequent_method['New York'])
+
+# Write this updated data out to csv file
+df.to_csv('data/processed/cleaned_data.csv', index=False)
